63bc8abbfaa864b6fcab090cdd040c456771e5e5
Burak <burakkahyabk@gmail.com>
2024-04-25 19:38:38 +0300

    created a new reusable component using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImage from './assets/react-core-concepts.png';
+import componenetsImage from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'Important'];
 
@@ -20,12 +21,33 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The Core UI Building block."
+              image={componenetsImage}
+            />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
